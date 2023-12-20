a906e54847749d58a9e576a336aa39fcf6a1a4f5
Basti <basti.kreuz@googlemail.com>
2023-12-20 18:24:23 +0100

    Add portfolio optimization jupyter notebook

--- src/finance_ml/porfolio_optimization/__init__.py
+++ src/finance_ml/porfolio_optimization/__init__.py
@@ -1,2 +1,2 @@
-from .hierarch_risk_parity import PortfolioOptimization
+from .portfolio_optimization import PortfolioOptimization
 from .plotting import show_dendogram, show_pie_charts, show_result_plot
